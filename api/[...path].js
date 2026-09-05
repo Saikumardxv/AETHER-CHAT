1e1e61e55261f2a933d05ff997496c7b9f64cf6b
Saikumar <your_github_email@example.com>
2026-09-05 08:19:24 +0530

    Fix production API routing

--- api/[...path].js
+++ api/[...path].js
@@ -4,6 +4,12 @@ import { connectDB } from '../server/config/db.js';
 const databaseReady = connectDB();
 
 export default async function handler(request, response) {
+  if (request.query?.route) {
+    const route = String(request.query.route).replace(/^\/+/, '');
+    const query = new URLSearchParams(request.query);
+    query.delete('route');
+    request.url = `/api/${route}${query.toString() ? `?${query}` : ''}`;
+  }
   await databaseReady;
   return app(request, response);
 }
